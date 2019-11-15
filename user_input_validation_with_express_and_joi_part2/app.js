2df5e430140b58164e4bf098a82f47e1c2b3e304
Eddy <edward.gonzalez9708@gmail.com>
2019-11-15 15:50:00 -0500

    Completed the user input validation with express and joi part 2.

---
+++ user_input_validation_with_express_and_joi_part2/app.js
@@ -0,0 +1,47 @@
+const Joi = require("Joi");
+
+const arrayString = [
+  "banana",
+  "bacon",
+  "cheese"
+];
+
+const arrayObjects = [
+  { example: "example1" },
+  { example: "example2" },
+  { example: "example3" }
+];
+
+const userInput = {
+  personalInfo: {
+    streetAddress: "123987987",
+    city: "kljlkajd",
+    state: "fl"
+  },
+  // preferences: arrayString
+  preferences: arrayObjects
+};
+
+const personalInfoSchema = Joi.object().keys({
+  streetAddress: Joi.string().trim().required(),
+  city: Joi.string().trim().required(),
+  state: Joi.string().trim().length(2).required()
+});
+
+// const preferencesSchema = Joi.array().items(Joi.string());
+const preferencesSchema = Joi.array().items(Joi.object().keys({
+  example: Joi.string().required()
+}));
+
+const schema = Joi.object().keys({
+  personalInfo: personalInfoSchema,
+  preferences: preferencesSchema
+});
+
+Joi.validate(userInput, schema, (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
